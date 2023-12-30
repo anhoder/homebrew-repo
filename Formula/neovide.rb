abb790695d3d1b7d0d619932ae94822dc3def538
anhoder <anhoderai@xiaoman.cn>
2023-12-30 17:12:12 +0800

    update

---
+++ Formula/neovide.rb
@@ -0,0 +1,14 @@
+class NeovimNightly < Formula
+  desc "This is a nightly neovim repo"
+  homepage "https://github.com/neovim/neovim"
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
+  version "nightly"
+  #sha256 :no_check
+
+  def install
+    lib.install Dir["lib/*"]
+    share.install Dir["share/*"]
+    bin.install "bin/nvim"
+    bin.install_symlink "nvim" => "neovim"
+  end
+end
