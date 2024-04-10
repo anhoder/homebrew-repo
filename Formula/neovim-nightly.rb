890732a295d0e351a6e99057536521fa8c2f8835
anhoder <anhoder@88.com>
2024-04-11 00:06:55 +0800

    fix(neovim-nightly): update download url

--- Formula/neovim-nightly.rb
+++ Formula/neovim-nightly.rb
@@ -1,9 +1,14 @@
 class NeovimNightly < Formula
   desc "This is a nightly neovim repo"
   homepage "https://github.com/neovim/neovim"
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
   version "nightly"
   #sha256 :no_check
+
+  if Hardware::CPU.arm?
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz"
+  else
+    url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz"
+  end
 
   def install
     lib.install Dir["lib/*"]
