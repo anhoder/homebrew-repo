cb28226c0534f5d1978e9ac4be711f33031533dc
anhoder <anhoder@88.com>
2025-11-12 15:11:08 +0800

    feat: add neovide nightly

---
+++ Casks/neovide-app-nightly.rb
@@ -0,0 +1,16 @@
+cask "neovide-app-nightly" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  url "https://github.com/neovide/neovide/releases/download/nightly/Neovide-#{arch}-apple-darwin.dmg"
+  name "Neovide"
+  desc "Neovim Client"
+  homepage "https://github.com/neovide/neovide"
+
+  app "Neovide.app"
+  binary "#{appdir}/Neovide.app/Contents/MacOS/neovide"
+
+  zap trash: [
+    "~/Library/Application Support/neovide",
+    "~/Library/Saved Application State/com.neovide.neovide.savedState",
+  ]
+end
